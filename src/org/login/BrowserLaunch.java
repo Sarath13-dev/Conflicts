29284b0d492b3a5de64f22fd5ef5a45604f4cbf0
Sarath13-dev <ramsssjrs@gmail.com>
2021-10-06 09:52:41 +0530

    New joiner B

--- src/org/login/BrowserLaunch.java
+++ src/org/login/BrowserLaunch.java
@@ -16,7 +16,8 @@ public class BrowserLaunch {
 		driver.get("https://www.facebook.com/");
 		driver.findElement(By.id("email")).sendKeys("ramsss");
 		driver.findElement(By.id("pass")).sendKeys("1234");
-		
+		System.out.println("New joiner B");
+		System.out.println("my work");
 		
 		
 		
